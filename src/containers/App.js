fb6b16b97ccfee5f0cd876d9697d18593bda7060
Victor Perez <vaperezr@unal.edu.co>
2017-12-18 22:09:44 -0500

    chore: adds react-router to the project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,40 @@
 import React, { Component } from 'react'
 import styles from './App.scss'
 import * as api from '../utils/api'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
 
   state = {
+    categories: [],
     posts: []
   }
 
   componentDidMount () {
-    api.getPosts().then((posts) => {
+    api.getPosts().then(posts => {
 			this.setState({ posts })
+    })
+
+    api.getCategories().then(categories => {
+      this.setState({ categories })
     })
   }
 
   render() {
     return (
-      <div className={styles.app}>
-        {
-          this.state.posts.map(post => (
-            <div key={ post.id}>
-              <h2>{ post.title }</h2>
-            </div>
-          ))
-        }
-      </div>
+      <Switch>
+        <Route exact path="/">
+        <div className={styles.app}>
+          {
+            this.state.posts.map(post => (
+              <div key={ post.id}>
+                <h2>{ post.title }</h2>
+              </div>
+            ))
+          }
+        </div>
+        </Route>
+      </Switch>
     );
   }
 }
