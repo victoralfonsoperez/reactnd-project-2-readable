6e8ed3607a8bcb8c781afb0d8cacaf897e40c1bf
Victor Perez <vicperez@cobogpgp3428.global.publicisgroupe.net>
2017-12-27 19:59:40 -0500

    feature: adds the ability to display the comments inside the details post view

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -9,6 +9,7 @@ import { postDeleter, currentPost } from '../actions'
 class PostDetail extends Component {
     state = {
         post: {},
+        comments: [],
         redirectToHome: false
     }
 
@@ -22,77 +23,102 @@ class PostDetail extends Component {
         let postid = this.props.location.pathname.replace(/^\/+/g, '')
         api.getSinglePost(postid)
             .then(currentpost => this.setState({ post: currentpost }))
+        api.getPostComments(postid)
+            .then(data => this.setState({ comments: data}))
     }
 
     render() {
 
         const { from } = this.props.location.state || '/'
-        const { post, redirectToHome } = this.state
+        const { post, redirectToHome, comments } = this.state
 
         return (
             <div>
-                <h3>{ post.title }</h3>
-                <FontAwesome.FaHeart/> <span className={styles.postvotescore}>{ post.voteScore }</span>
-                <div className={styles.postbody}>
-                    <p>
-                        { post.body }
-                    </p>
-                </div>
-                <div className={styles.postauthor}>
-                    <figure className={styles.postauthorpicture}>
-                    <img src="https://randomuser.me/api/portraits/lego/1.jpg" alt={post.author}/>
-                    <figcaption className={styles.postauthorfigcaption}>
-                        <span>by:</span> {post.author}<br/>
-                        <span>on:</span> <Link to={`/${post.category}`}>{post.category}</Link>
-                        </figcaption>
-                    </figure>
-                </div>
-                <div className={styles.postdate}><FontAwesome.FaCalendarO />
-                    <span className={styles.postdatevalue}>{ new Date(post.timestamp).toLocaleString() }</span>
-                </div>
+                <div className={styles.postdetail}>
+                    <div className={styles.postdetailhead}>
+                        <h3 className={styles.postdetailtitle}>{ post.title }</h3>
+                        <div className={styles.postdetailvote}>
+                            <FontAwesome.FaHeart/> <span className={styles.postvotescore}>{ post.voteScore }</span>
+                        </div>
+                    </div>
+                    <div className={styles.postbody}>
+                        <p>
+                            { post.body }
+                        </p>
+                    </div>
+                    
+                    <div className={styles.postdetaildata}>
+                    <div className={styles.postauthor}>
+                        <figure className={styles.postauthorpicture}>
+                            <img src="https://randomuser.me/api/portraits/lego/1.jpg" alt={post.author}/>
+                            <figcaption className={styles.postauthorfigcaption}>
+                                <span>by:</span> {post.author}<br/>
+                                <span>on:</span> <Link to={`/${post.category}`}>{post.category}</Link>
+                                </figcaption>
+                            </figure>
+                        </div>
+                        <div className={styles.postdate}><FontAwesome.FaCalendarO />
+                            <span className={styles.postdatevalue}>{ new Date(post.timestamp).toLocaleString() }</span>
+                        </div>
+                    </div>
 
-                <div className={styles.postcommentcount}><FontAwesome.FaCommentsO />
-                    <span>Comments</span>
-                    <span className="post-comment-count-value">{ post.commentCount }</span>
-                </div>
+                    <div className={styles.postcommentcount}><FontAwesome.FaCommentsO />
+                        <span>Comments</span>
+                        <span className="post-comment-count-value">{ post.commentCount }</span>
+                    </div>
 
-                <div className={styles.postactions}>
-                    <NavLink
-                        to={`/edit/${post.id}`}
-                        className={styles.editbutton}
-                        onClick={() => {this.setActualPost(post)}}>
-                        <FontAwesome.FaEdit/>
-                    </NavLink>
+                    <div className={styles.postactions}>
+                        <NavLink
+                            to={`/edit/${post.id}`}
+                            className={styles.editbutton}
+                            onClick={() => {this.setActualPost(post)}}>
+                            <FontAwesome.FaEdit/>
+                        </NavLink>
 
-                    <button
-                        className={styles.deletebutton}
-                        onClick={() => this.deletePost(post.id)}>
-                        <FontAwesome.FaTrashO/>
-                    </button>
+                        <button
+                            className={styles.deletebutton}
+                            onClick={() => this.deletePost(post.id)}>
+                            <FontAwesome.FaTrashO/>
+                        </button>
 
-                    <button
-                        className={styles.downvotebutton}
-                        disabled={this.state.isDownVoteButtonDisabled}>
-                        <FontAwesome.FaThumbsODown/>
-                    </button>
+                        <button
+                            className={styles.downvotebutton}
+                            disabled={this.state.isDownVoteButtonDisabled}>
+                            <FontAwesome.FaThumbsODown/>
+                        </button>
 
-                    <button
-                        className={styles.upvotebutton}
-                        disabled={this.state.isUpVoteButtonDisabled}>
-                        <FontAwesome.FaThumbsOUp/>
-                    </button>
+                        <button
+                            className={styles.upvotebutton}
+                            disabled={this.state.isUpVoteButtonDisabled}>
+                            <FontAwesome.FaThumbsOUp/>
+                        </button>
 
-                    <NavLink
-                        to="/comment"
-                        className={styles.commentbutton}>
-                        <FontAwesome.FaComment/>
-                    </NavLink>
+                        <NavLink
+                            to="/comment"
+                            className={styles.commentbutton}>
+                            <FontAwesome.FaComment/>
+                        </NavLink>
+                    </div>
+                    {
+                        redirectToHome && (
+                            <Redirect to={from || '/'}/>
+                        )
+                    }
                 </div>
                 {
-					redirectToHome && (
-						<Redirect to={from || '/'}/>
-					)
-				}
+                    comments && comments.map(comment => (
+                        <div>
+                            <div>{comment.author}</div>
+                            <div>{comment.body}</div>
+                            <div>{comment.deleted}</div>
+                            <div>{comment.id}</div>
+                            <div>{comment.parentDeleted}</div>
+                            <div>{comment.parentId}</div>
+                            <div>{comment.timestamp}</div>
+                            <div>{comment.voteScore}</div>
+                        </div>
+                    ))
+                }
             </div>
         )
     }
